aaca3a518e6ee3f0075d5da4f9707a09ed603003
yassine bouzagou <yassine.bouzagou.02@gmail.com>
2025-10-29 18:03:33 +0000

    no aparecen bien los mensajes el entra algun chat y El problema visual q

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -3,7 +3,7 @@ import { Messages } from '@/components/dashboard/messages';
 export default function DashboardPage() {
   return (
     <div className="h-screen flex flex-col">
-       <div className="flex-grow overflow-hidden">
+       <div className="flex-1 overflow-hidden">
         <Messages />
        </div>
     </div>
