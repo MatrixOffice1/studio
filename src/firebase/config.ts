bea7363a03c7b7c8458198a677099b99c4ac4d15
yassine bouzagou <yassine.bouzagou.02@gmail.com>
2025-10-29 15:10:10 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-7690246651-b132f",
+  "appId": "1:163566537165:web:a8cc4545105307a73cdc84",
+  "apiKey": "AIzaSyBRWl7tI5q72sYiqIUYzfrqTt4CCSuirLc",
+  "authDomain": "studio-7690246651-b132f.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "163566537165"
+};
