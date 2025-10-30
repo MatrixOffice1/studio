f1ab92c63e0306babf61d02b6011b1e336ef184e
yassine bouzagou <yassine.bouzagou.02@gmail.com>
2025-10-30 21:33:29 +0000

    vuelve poner el logo en login bien que no sale bien, era este ( https://

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -45,7 +45,7 @@ export default function LoginPage() {
     <div className="flex flex-col items-center justify-center min-h-screen bg-background p-4">
       <div className="w-full max-w-sm text-center">
         <Image
-          src="https://i.postimg.cc/LXVKqFqv/df.png"
+          src="https://i.postimg.cc/kGtCyQCD/logo2.png"
           alt="Logo"
           width={400}
           height={400}
